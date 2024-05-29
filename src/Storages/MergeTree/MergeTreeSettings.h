c09f9d86abef230d5b3b5f99301b9f4b81925b0a
zhongyuankai <872237106@qq.com>
2024-05-29 10:21:49 +0800

    [Clickhouse-58]Added `ignore_merge_type` setting to be compatible with lower versions

--- src/Storages/MergeTree/MergeTreeSettings.h
+++ src/Storages/MergeTree/MergeTreeSettings.h
@@ -162,6 +162,7 @@ struct Settings;
     M(Bool, allow_nullable_key, false, "Allow Nullable types as primary keys.", 0) \
     M(Bool, remove_empty_parts, true, "Remove empty parts after they were pruned by TTL, mutation, or collapsing merge algorithm.", 0) \
     M(Bool, assign_part_uuids, false, "Generate UUIDs for parts. Before enabling check that all replicas support new format.", 0) \
+    M(Bool, ignore_merge_type, false, "Compatible with lower version replica, the merge type cannot be recognized.", 0) \
     M(Int64, max_partitions_to_read, -1, "Limit the max number of partitions that can be accessed in one query. <= 0 means unlimited. This setting is the default that can be overridden by the query-level setting with the same name.", 0) \
     M(UInt64, max_concurrent_queries, 0, "Max number of concurrently executed queries related to the MergeTree table (0 - disabled). Queries will still be limited by other max_concurrent_queries settings.", 0) \
     M(UInt64, min_marks_to_honor_max_concurrent_queries, 0, "Minimal number of marks to honor the MergeTree-level's max_concurrent_queries (0 - disabled). Queries will still be limited by other max_concurrent_queries settings.", 0) \
